6a38d7efd44228bb60de89e03050f61526db54ce
gerrykwok <evakwok1981@hotmail.com>
2018-06-29 16:12:06 +0800

    加入七牛设置zone的注释代码

--- cocosext/src/plmext/android/UploadHead.java
+++ cocosext/src/plmext/android/UploadHead.java
@@ -18,6 +18,27 @@ import com.qiniu.android.storage.UploadOptions;
 
 import com.xishanju.defold.cocosext.*;
 
+//        //————http上传,指定zone的具体区域——
+//        //Zone.zone0:华东
+//        //Zone.zone1:华北
+//        //Zone.zone2:华南
+//        //Zone.zoneNa0:北美
+//        //———http上传，自动识别上传区域——
+//        //Zone.httpAutoZone
+//        //———https上传，自动识别上传区域——
+//        //Zone.httpsAutoZone
+//
+//        Configuration config = new Configuration.Builder()
+//                .chunkSize(256 * 1024)  //分片上传时，每片的大小。 默认256K
+//                .putThreshhold(512 * 1024)  // 启用分片上传阀值。默认512K
+//                .connectTimeout(10) // 链接超时。默认10秒
+//                .responseTimeout(60) // 服务器响应超时。默认60秒
+//                .recorder(null)  // recorder分片上传时，已上传片记录器。默认null
+//                .recorder(null, null)  // keyGen 分片上传时，生成标识符，用于片记录器区分是那个文件的上传记录
+//                .zone(Zone.zone2) // 设置区域，指定不同区域的上传域名、备用域名、备用IP。
+//                .build();
+//        this.uploadManager = new UploadManager(config);
+
 public class UploadHead
 {
 	private static final int AVATAR_UPLOAD_RES_SUCCES = 1;
