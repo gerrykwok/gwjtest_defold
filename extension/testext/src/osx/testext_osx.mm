1747ea3968a270e518b9a5cc4f9814bbf47a8866
gerrykwok19xx <evakwok@21cn.com>
2019-02-14 17:28:30 +0800

    testext:center window

--- extension/testext/src/osx/testext_osx.mm
+++ extension/testext/src/osx/testext_osx.mm
@@ -1,5 +1,6 @@
 #if defined(DM_PLATFORM_OSX)
 
+#include <AppKit/AppKit.h>
 #include "../testext.h"
 
 int test_callnative(lua_State *L)
@@ -9,28 +10,12 @@ int test_callnative(lua_State *L)
 
 int test_centerWindow(lua_State *L)
 {
-	float x,y;
-	int w,h;
 	NSWindow* window = dmGraphics::GetNativeOSXNSWindow();
-	NSRect frame = window.screen.frame;
-	w = frame.size.width;
-	h = frame.size.height;
-
-	x = floorf(frame.origin.x + (frame.size.width - w) * 0.5f);
-
-	float win_y;
-	win_y = floorf(frame.origin.y + (frame.size.height - h) * 0.5f);
-
-	NSView* view = dmGraphics::GetNativeOSXNSView();
-	NSRect viewFrame = [view convertRect: view.bounds toView: nil];
+	NSRect screenFrame = window.screen.frame;
 	NSRect windowFrame = window.frame;
-	NSRect rect = NSMakeRect(
-		x - viewFrame.origin.x,
-		win_y - viewFrame.origin.x,
-		w + viewFrame.origin.x + windowFrame.size.width - viewFrame.size.width,
-		h + viewFrame.origin.y + windowFrame.size.height - viewFrame.size.height
-	);
-	[window setFrame: rect display:YES];
+
+	[window setFrameOrigin:NSMakePoint((screenFrame.size.width-windowFrame.size.width)/2, (screenFrame.size.height-windowFrame.size.height)/2)];
+
 	return 0;
 }
 
