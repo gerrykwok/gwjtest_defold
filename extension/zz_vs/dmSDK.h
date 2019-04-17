2f14ac4e1cecfad61d31962c9416ae6042980c91
gerrykwok <evakwok1981@hotmail.com>
2019-04-17 11:53:11 +0800

    为VS中的C++代码提示加入相应的头文件

---
+++ extension/zz_vs/dmSDK.h
@@ -0,0 +1,8 @@
+//////////////////////////////////////////////////////////////////////////
+
+void dmLogDebug(const char *fmt, ...);
+void dmLogError(const char *fmt, ...);
+void dmLogFatal(const char *fmt, ...);
+void dmLogInfo(const char *fmt, ...);
+void dmLogUserDebug(const char *fmt, ...);
+void dmLogWarning(const char *fmt, ...);
